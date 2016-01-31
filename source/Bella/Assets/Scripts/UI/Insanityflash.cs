c35950897ecb1554a68360c78ff0849cdf929652
Squirells <thesquirells@live.com>
2016-01-31 14:09:14 -0500

    imported UI prefab. into test level, And level 1

---
+++ source/Bella/Assets/Scripts/UI/Insanityflash.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Insanityflash : MonoBehaviour {
+    public Player uiPlayerTracker;
+    // Use this for initialization
+    void Start () {
+        GetComponent<Image>().CrossFadeAlpha((float)0, (float)0, false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	if(uiPlayerTracker.insanity >= uiPlayerTracker.dangerThreshold)
+        {
+           GetComponent<Image>().CrossFadeAlpha((float)1.0, (float)2.0,false);
+        }
+	}
+}

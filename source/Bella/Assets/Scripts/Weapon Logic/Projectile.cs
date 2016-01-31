fda61f8f49d98c3726171e004bb2010f1af1549a
BewareOfNargles <kimferguson13@gmail.com>
2016-01-31 13:56:49 -0500

    Revert "Un-reverting Projectile."
    This reverts commit 530d70707771b4270f06d964026f977a02761ed6.

--- source/Bella/Assets/Scripts/Weapon Logic/Projectile.cs
+++ source/Bella/Assets/Scripts/Weapon Logic/Projectile.cs
@@ -3,52 +3,22 @@ using System.Collections;
 
 public class Projectile : MonoBehaviour
 {
-    public int speed = 5;
     public int range = 1;
-
-    [HideInInspector]
-    public int directionMultiplier = 1;
-
-    public int maxLifetime = 10;    // Just in case there's nothing to collide with, kill the projectile after this many seconds
-    private float lifeTimer = 0.0f;
-
-    private Rigidbody2D m_rigidBody2D;
-    private Vector2 m_velocity;
 
 	// Use this for initialization
 	void Start()
 	{
-        m_rigidBody2D = GetComponent<Rigidbody2D>();
-        m_velocity = new Vector2(speed * directionMultiplier, 0.0f);
-
-        transform.localScale *= directionMultiplier;
+		
 	}
 	
 	// Update is called once per frame
 	void Update()
 	{
-        // TODO: Remove this line once speed has been polished
-        m_velocity.x = speed * directionMultiplier;
-
-        lifeTimer += Time.deltaTime;
-        if (lifeTimer >= maxLifetime)
-        {
-            Destroy(gameObject);
-        }
+		
 	}
 
     void FixedUpdate()
     {
-        m_rigidBody2D.velocity = m_velocity;
-    }
 
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        // We don't care if the bullet collided with the player.
-        Player player = other.gameObject.GetComponent<Player>();
-        if (player == null)
-        {
-            Destroy(gameObject);
-        }
     }
 }

405d282a2b6f162570a6ff35bcc3440e3bf2685c
BewareOfNargles <kimferguson13@gmail.com>
2016-01-31 13:05:54 -0500

    Removing crouch detection, further preliminary "pacing" setup for NPCs.

--- source/Bella/Assets/Scripts/AI/Platformer2DPacingControl.cs
+++ source/Bella/Assets/Scripts/AI/Platformer2DPacingControl.cs
@@ -1,13 +1,21 @@
 ﻿using UnityEngine;
+using UnityStandardAssets._2D;
+using System;
 using System.Collections;
 
+[RequireComponent(typeof(PlatformerCharacter2D))]
 public class Platformer2DPacingControl : MonoBehaviour
 {
+    public int maxPaceDistance = 10;    // Max distance the character will pace in one direction if no obstacles are in the way
+
+    private PlatformerCharacter2D character;
+    private Transform forwardGroundCheck;
 
 	// Use this for initialization
 	void Start()
 	{
-		
+        character = GetComponent<PlatformerCharacter2D>();
+        forwardGroundCheck = transform.Find("ForwardGroundCheck");
 	}
 	
 	// Update is called once per frame
@@ -15,4 +23,9 @@ public class Platformer2DPacingControl : MonoBehaviour
 	{
 		
 	}
+
+    void FixedUpdate()
+    {
+
+    }
 }

33298c0448f6650a0ae5ceac7ab0e28059dfe6e2
Will Teixeira <willrteixeira@gmail.com>
2016-01-31 13:20:57 -0500

    main menu and options and win level menu

--- source/Bella/Assets/Scripts/UI/MainMenu.cs
+++ source/Bella/Assets/Scripts/UI/MainMenu.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
@@ -15,16 +16,16 @@ public class MainMenu : MonoBehaviour {
 
     public void ExitGame()
     {
-
+        Application.Quit();
     }
 
     public void StartGame()
     {
-
+        SceneManager.LoadScene("level1");
     }
 
     public void OpenOptions()
     {
-
+        SceneManager.LoadScene("Options");
     }
 }
---
+++ source/Bella/Assets/Scripts/UI/Options.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Options : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void SetFullscreen()
+    {
+        Resolution r = Screen.currentResolution;
+        Screen.SetResolution(r.width, r.height, false);
+    }
+
+    public void SetWindowed()
+    {
+        Resolution r = Screen.currentResolution;
+        Screen.SetResolution(r.width, r.height, false);
+    }
+}

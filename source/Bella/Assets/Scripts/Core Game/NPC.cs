a79c2885ba8e759d3578f52a22a7b9fc3c50bb7c
BewareOfNargles <kimferguson13@gmail.com>
2016-02-21 21:09:35 -0500

    Now NPCs don't re-die.

--- source/Bella/Assets/Scripts/Core Game/NPC.cs
+++ source/Bella/Assets/Scripts/Core Game/NPC.cs
@@ -25,19 +25,22 @@ public class NPC : MonoBehaviour
     [ContextMenu("Die")]
     public void Die()
     {
-        IsDead = true;
+        if (!IsDead)
+        {
+            IsDead = true;
 
-        // Trigger death animation, kill behaviors
-        animator.SetTrigger("OnDeath");
-        behaviorManager.OnDeath();
+            // Trigger death animation, kill behaviors
+            animator.SetTrigger("OnDeath");
+            behaviorManager.OnDeath();
 
-        // Remove view cone
-        Destroy(transform.FindChild("ViewCone").gameObject);
+            // Remove view cone
+            Destroy(transform.FindChild("ViewCone").gameObject);
 
-        // Knock the NPC's hat off! Muahaha
-        Transform hat = transform.FindChild("hat");
-        hat.parent = null;
-        Rigidbody2D hatRigidBody = hat.gameObject.AddComponent<Rigidbody2D>();
-        hatRigidBody.AddForce(new Vector2(2.0f, 1.0f), ForceMode2D.Impulse);
+            // Knock the NPC's hat off! Muahaha
+            Transform hat = transform.FindChild("hat");
+            hat.parent = null;
+            Rigidbody2D hatRigidBody = hat.gameObject.AddComponent<Rigidbody2D>();
+            hatRigidBody.AddForce(new Vector2(2.0f, 1.0f), ForceMode2D.Impulse);
+        }
     }
 }
